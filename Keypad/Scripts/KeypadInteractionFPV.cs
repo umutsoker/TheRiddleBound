ab0649aeac8a15e57749d31128704cf55d975480
umutsoker <89361102+umutsoker@users.noreply.github.com>
2025-04-10 01:44:38 +0300

    Update KeypadInteractionFPV.cs

--- Keypad/Scripts/KeypadInteractionFPV.cs
+++ Keypad/Scripts/KeypadInteractionFPV.cs
@@ -2,25 +2,35 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace NavKeypad { 
-public class KeypadInteractionFPV : MonoBehaviour
+namespace NavKeypad 
 {
-    private Camera cam;
-    private void Awake() => cam = Camera.main;
-    private void Update()
+    public class KeypadInteractionFPV : MonoBehaviour
     {
-        var ray = cam.ScreenPointToRay(Input.mousePosition);
+        private Camera cam; // Birinci şahıs kamerası
 
-        if (Input.GetMouseButtonDown(0))
+        // Başlangıçta ana kamerayı bul
+        private void Awake() => cam = Camera.main;
+
+        // Her frame'de çalışan güncelleme metodu
+        private void Update()
         {
-            if (Physics.Raycast(ray, out var hit))
+            // Fare pozisyonundan bir ışın (ray) oluştur
+            var ray = cam.ScreenPointToRay(Input.mousePosition);
+
+            // Sol tıklama yapıldığında
+            if (Input.GetMouseButtonDown(0))
             {
-                if (hit.collider.TryGetComponent(out KeypadButton keypadButton))
+                // Işın bir nesneye çarparsa
+                if (Physics.Raycast(ray, out var hit))
                 {
-                    keypadButton.PressButton();
+                    // Çarptığımız nesnede KeypadButton komponenti var mı?
+                    if (hit.collider.TryGetComponent(out KeypadButton keypadButton))
+                    {
+                        // Varsa butona basma fonksiyonunu tetikle
+                        keypadButton.PressButton();
+                    }
                 }
             }
         }
     }
-}
 }
